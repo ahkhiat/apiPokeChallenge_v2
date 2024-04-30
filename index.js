436878023b9a8a0b378b2e272d17e7f961a43bb7
Thierry Leung <145972274+ahkhiat@users.noreply.github.com>
2024-04-30 17:01:44 +0200

    jesaispas

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+const express = require('express')
+const app = express()
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+mongoose.connect(process.env.MONGODB_PATH)
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+const cartePokemonSchema = new mongoose.Schema({
+  nom: { type: String, required: true },
+  type: { type: String, required: true },
+  imageSrc: { type: String, required: true }
+});
+
+const CartePokemonModel = mongoose.model('CartePokemon', cartePokemonSchema);
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+app.post('/', function (req, res) {
+  const newPokemon = new CartePokemon(req.body);
+  try {
+    newPokemon.save();
+    console.log('Nouveau Pokémon enregistré avec succès !');
+  } catch (error) {
+    console.error("Erreur lors de l'enregistrement du Pokémon :", error);
+  }
+  
+
+})
+
+
 
 
 
